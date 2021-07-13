1501ea2b29a3657a98d6e9a1c07994afff962957
Pradeep Singh <pradeep@dotvik.com>
2021-07-13 06:06:32 +0530

    removing Manifest.permission.MANAGE_EXTERNAL_STORAGE, as Andrroid playstore is very strict on this permission for Android 11.

--- app/src/main/java/com/sdbiosensor/covicatch/screens/FormActivity.java
+++ app/src/main/java/com/sdbiosensor/covicatch/screens/FormActivity.java
@@ -801,23 +801,18 @@ public class FormActivity extends BaseActivity implements View.OnClickListener{
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
             if (ActivityCompat.shouldShowRequestPermissionRationale
                     (this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE)
                     || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
                 // check again permission
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
-                                , Manifest.permission.CAMERA
-                                , Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                                , Manifest.permission.CAMERA},
                         CAMERA_PERMISSIONS_CODE);
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE
-                                , Manifest.permission.CAMERA
-                                , Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                                , Manifest.permission.CAMERA},
                         CAMERA_PERMISSIONS_CODE);
                 // Grant Permission
             }
--- app/src/main/java/com/sdbiosensor/covicatch/screens/HistoryActivity.java
+++ app/src/main/java/com/sdbiosensor/covicatch/screens/HistoryActivity.java
@@ -102,23 +102,15 @@ public class HistoryActivity extends BaseActivity{
     private void checkPermissions(String id) {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
             if (ActivityCompat.shouldShowRequestPermissionRationale
-                    (this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE)
-                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                    (this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 // check again permission
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
-                                , Manifest.permission.CAMERA
-                                , Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         CAMERA_PERMISSIONS_CODE);
             } else {
-                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE
-                                , Manifest.permission.CAMERA
-                                , Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         CAMERA_PERMISSIONS_CODE);
                 // Grant Permission
             }
